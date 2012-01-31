ae1b4b5a900225e577214c736b14e4257884d9b7
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-01-31 23:12:47 +0900

    Exclude vendor/* include in src.php.

--- src.php
+++ src.php
@@ -6,8 +6,3 @@ require __DIR__ . '/src/AnnotationAccess.php';
 require __DIR__ . '/src/ReflectionMethod.php';
 require __DIR__ . '/src/ReflectionClass.php';
 require __DIR__ . '/src/ReflectionProperty.php';
-
-require __DIR__ . '/vendor/Doctrine/Common/lib/Doctrine/Common/ClassLoader.php';
-$commonLoader = new \Doctrine\Common\ClassLoader('Doctrine\Common', __DIR__ . '/vendor/Doctrine/Common/lib');
-$commonLoader->register();
-
--- tests/src.php
+++ tests/src.php
@@ -1,4 +1,9 @@
 <?php
+require dirname(__DIR__) . '/vendor/Doctrine/Common/lib/Doctrine/Common/ClassLoader.php';
+
+$commonLoader = new \Doctrine\Common\ClassLoader('Doctrine\Common', dirname(__DIR__) . '/vendor/Doctrine/Common/lib');
+$commonLoader->register();
+
 require __DIR__ . '/Mock/Annotation/Bar.php';
 require __DIR__ . '/Mock/Annotation/Foo.php';
 require __DIR__ . '/Mock/Class/User.php';
